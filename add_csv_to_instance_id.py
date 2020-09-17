adb83d9d5aa2365c57ded3b4e5ca4a8fff4c6f72
lli259 <37081135+lli259@users.noreply.github.com>
2020-09-16 23:26:51 -0400

    Add files via upload

--- add_csv_to_instance_id.py
+++ add_csv_to_instance_id.py
@@ -3,13 +3,14 @@ import pandas as pd
 
 filename=sys.argv[1]
 df=pd.read_csv(filename)
-df=df.set_index('ins')
+#df=df.set_index('ins')
 
 insts=df['ins']
 instscsv=[i+'.csv' for i in insts]
-df['inscsv']=instscsv
-df=df.set_index('inscsv')
+df=df.set_index('ins')
+df['ins']=instscsv
+df=df.set_index('ins')
 
-df.to_csv("new_"+filename)
+df.to_csv("new/"+filename)
 
 
